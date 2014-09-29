6774a0bf18ee1b450c4bfc8ea62c58733e512740
Low Ching Nam Raymon <iamraymondlow@gmail.com>
2014-09-29 21:54:26 +0800

    Mark item class completed

--- JustAddIt/CommandLogic/MarkItemDone.cpp
+++ JustAddIt/CommandLogic/MarkItemDone.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "MarkItemDone.h"
+#include "Item.h"
 
 
 MarkItemDone::MarkItemDone(vector<Item>::iterator item) {
@@ -12,5 +13,15 @@ MarkItemDone::~MarkItemDone() {
 }
 
 vector<string> MarkItemDone::execute() {
+	markItem();
+	displaySuccessMessage();
+	return CommandLogic::outputMessageStorage;
+}
 
+void markItem() {
+	//_item->_isDone = true;
+}
+
+void displaySuccessMessage() {
+	CommandLogic::outputMessageStorage.push_back("Successfully marked!");
 }
