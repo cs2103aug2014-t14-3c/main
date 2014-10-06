cf2f6aaa3b804675a60400424480e93e05935a72
happyjimmy <12100977d@connect.polyu.hk>
2014-10-06 13:52:35 +0800

    Waiting for remaking of the search part!
    Waiting for remaking of the search part!

--- JustAddIt/JustAddIt/CmdSearch.cpp
+++ JustAddIt/JustAddIt/CmdSearch.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 #include "CmdSearch.h"
 
-CmdSearch::CmdSearch(vector<Item>::iterator itemPtr) {
-	_itemPtr = itemPtr;
+CmdSearch::CmdSearch(string keyword) {
 }
 
 CmdSearch::~CmdSearch(void) {
 }
 
 vector<string> CmdSearch::execute() {
+	return temporaryStorage;
 }
--- JustAddIt/JustAddIt/CmdSearch.h
+++ JustAddIt/JustAddIt/CmdSearch.h
@@ -10,7 +10,7 @@ private:
 	vector<string> temporaryStorage;
 	vector<Item>::iterator _itemPtr;
 public:
-	CmdSearch(vector<Item>::iterator itemPtr);
+	CmdSearch(string keyword);
 	~CmdSearch(void);
 
 	vector<string> CmdSearch::execute();
