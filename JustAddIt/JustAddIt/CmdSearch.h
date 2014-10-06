13c6165e2f7c7409bd3672fa6544f150bb35ace3
happyjimmy <12100977d@connect.polyu.hk>
2014-10-06 13:49:16 +0800

    Waiting for remaking of the search part!
    Waiting for remaking of the search part!

--- JustAddIt/JustAddIt/CmdSearch.h
+++ JustAddIt/JustAddIt/CmdSearch.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "Command.h"
+#include "ItemBank.h"
 
 class CmdSearch :
-	public Command {
+	public Command 
+{
 private:
 	string itemsToBeSearched;
 	vector<string> temporaryStorage;
+	vector<Item>::iterator _itemPtr;
 public:
-	CmdSearch(string keyword);
+	CmdSearch(vector<Item>::iterator itemPtr);
 	~CmdSearch(void);
 
-	string execute();
+	vector<string> CmdSearch::execute();
 };
 
