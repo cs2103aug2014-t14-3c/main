2555bb7ef2809d3b5d10b3241da7dd7dd5b5a1ef
happyjimmy <12100977d@connect.polyu.hk>
2014-10-21 20:11:22 +0800

    Unfinished search
    Unfinished search

--- JustAddIt/JustAddIt/CmdSearch.cpp
+++ JustAddIt/JustAddIt/CmdSearch.cpp
@@ -9,16 +9,23 @@ CmdSearch::~CmdSearch(void) {
 }
 
 vector<string> CmdSearch::execute() {
-	int sizeOfItemList = ItemBank::bank.size();
+	ifstream fin("JustAddIt_ItemBank.txt");
+	string itemsToBeSearched;
+	while(getline(fin, itemsToBeSearched)) {
+		temporaryStorage.push_back(itemsToBeSearched);
+	}
+
+	int sizeOfItemList = temporaryStorage.size();
 	for (int i = 0; i < sizeOfItemList; i++) {
-		for (int start = 0; start <= (signed)ItemBank::bank[i]->getTitle().length(); start++) {
-				if (!((temporaryStorage[i]).substr(start, temporaryStorage[i].length())).find(searchKeyword)) {
-					//showUser(temporaryStorage[i] + "\n");
-					contentAfterSearching.push_back(temporaryStorage[i]);
-					break;
-				}
+		for (int start = 0; start <= temporaryStorage[i].length(); start++) {
+			if (!((temporaryStorage[i]).substr(start, temporaryStorage[i].length())).find("")) {
+				//Put the keyword you want to search inside the double quotation marks of .find("")
+				contentAfterSearching.push_back(temporaryStorage[i]);
+				break;
+			}
 		}
 	}
+
 	for (int i = 0; i < sizeOfItemList; i++) {
 		temporaryStorage.pop_back();
 	}
