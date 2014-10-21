f578b3c9d497cd2631522754cbe9c2b449e3b880
Low Ching Nam Raymon <iamraymondlow@gmail.com>
2014-10-22 00:50:45 +0800

    previous errors debugged

--- JustAddIt/JustAddIt/CmdSearch.cpp
+++ JustAddIt/JustAddIt/CmdSearch.cpp
@@ -9,29 +9,6 @@ CmdSearch::~CmdSearch(void) {
 }
 
 vector<string> CmdSearch::execute() {
-<<<<<<< Updated upstream
-	ifstream fin("JustAddIt_ItemBank.txt");
-	string itemsToBeSearched;
-	while(getline(fin, itemsToBeSearched)) {
-		temporaryStorage.push_back(itemsToBeSearched);
-	}
-
-	int sizeOfItemList = temporaryStorage.size();
-	for (int i = 0; i < sizeOfItemList; i++) {
-		for (int start = 0; start <= temporaryStorage[i].length(); start++) {
-			if (!((temporaryStorage[i]).substr(start, temporaryStorage[i].length())).find("")) {
-				//Put the keyword you want to search inside the double quotation marks of .find("")
-				contentAfterSearching.push_back(temporaryStorage[i]);
-				break;
-			}
-		}
-	}
-
-	for (int i = 0; i < sizeOfItemList; i++) {
-		temporaryStorage.pop_back();
-	}
-	return contentAfterSearching;
-=======
 	vector<Item*>tasksToBeDisplayed;
 	vector<Item*>deadlinesToBeDisplayed;
 	vector<Item*>eventsToBeDisplayed;
@@ -47,5 +24,4 @@ vector<string> CmdSearch::execute() {
 	OutputControl::setCurrentScreen(OutputControl::CurrentScreenType::SEARCH_RESULTS_SCREEN);
 
 	return outputMessageStorage;
->>>>>>> Stashed changes
 }
