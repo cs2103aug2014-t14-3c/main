6c6195afd68fdd53bc596c477c36ac4e6861bc2f
Leon <ljj2020@gmail.com>
2014-10-30 12:06:11 +0800

    fixed the unit test for executor. test success now

--- JustAddIt/UnitTestLeon/ParserTest.cpp
+++ JustAddIt/UnitTestLeon/ParserTest.cpp
@@ -191,12 +191,12 @@ namespace UnitTestLeon
 			//DisplayScreenConstructor* myDSC = DisplayScreenConstructor::getInstance();
 			//OutputControl* myOC = new OutputControl();
 
-			myExec->execute("add wake up on 29 Oct at 7 #category (more info)");
+			myExec->execute("add wake up on 30 Dec at 7 #category (more info)");
 
 		
 			Assert::AreEqual("wake up", OutputControl::getItemAddr(1)->getTitle().c_str());
-			Assert::AreEqual("Wednesday 29 Oct 07:00AM", OutputControl::getItemAddr(1)->getStartDateInString().c_str());
-			Assert::AreEqual("Wednesday 29 Oct 08:00AM", OutputControl::getItemAddr(1)->getEndDateInString().c_str());
+			Assert::AreEqual("Tuesday 30 Dec 07:00AM", OutputControl::getItemAddr(1)->getStartDateInString().c_str());
+			Assert::AreEqual("Tuesday 30 Dec 08:00AM", OutputControl::getItemAddr(1)->getEndDateInString().c_str());
 			Assert::AreEqual("category", OutputControl::getItemAddr(1)->getCategory().c_str());
 			Assert::AreEqual("more info", OutputControl::getItemAddr(1)->getDescription().c_str());
 			Assert::AreEqual("Low", OutputControl::getItemAddr(1)->getPriorityInString().c_str());
