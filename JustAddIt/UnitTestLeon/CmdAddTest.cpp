d3a45dc1203168fb563e3cccd628397b095a21af
happyjimmy <12100977d@connect.polyu.hk>
2014-11-09 02:55:17 +0800

    Added one more simple unit test for cmdAdd
    Added one more simple unit test for cmdAdd

--- JustAddIt/UnitTestLeon/CmdAddTest.cpp
+++ JustAddIt/UnitTestLeon/CmdAddTest.cpp
@@ -3,9 +3,6 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-const string FILL = "This is a random filler text to test code";
-const string DATE = "Saturday 15 Nov 2014 08:00AM";
-
 namespace UnitTest
 {		
 	TEST_CLASS(CmdAdd)
@@ -15,23 +12,36 @@ namespace UnitTest
 		TEST_METHOD(CmdAddTest) // all positive test case *using multiple input
 		{
 			Item* itemPtr = new Item;
+			Item* itemPtr2 = new Item;
 			ItemBank* itemPointer = ItemBank::getInstance();
 
-			itemPtr->setTitle("finish JustAddIt");
-			itemPtr->setDescription("cs2103 project");
-			itemPtr->setStartDate(4, 8);
-			itemPtr->setStartTime(8, 0);
-			itemPtr->setEndDate(10, 11);
-			itemPtr->setEndTime(12, 0);
-			itemPtr->setPriority("High");
-			itemPtr->setCategory("Project");
-			itemPtr->setVenue("PGP");
-			
-			itemPointer -> addToBank(itemPtr);
+			itemPtr -> setTitle("finish JustAddIt");
+			itemPtr -> setDescription("cs2103 project");
+			itemPtr -> setStartDate(4, 8);
+			itemPtr -> setStartTime(8, 0);
+			itemPtr -> setEndDate(10, 11);
+			itemPtr -> setEndTime(12, 0);
+			itemPtr -> setPriority("High");
+			itemPtr -> setCategory("Project");
+			itemPtr -> setVenue("PGP");
 
-			Assert::AreEqual(itemPointer->getBankSize(), 1);
-			
-			itemPointer->clearBank();
+			Assert::AreEqual(itemPointer -> addToBank(itemPtr), false);
+			Assert::AreEqual(itemPointer -> getBankSize(), 1);
+
+			itemPtr2 -> setTitle("finish network assignment");
+			itemPtr2 -> setDescription("cs2105 assignment");
+			itemPtr2 -> setStartDate(4, 8);
+			itemPtr2 -> setStartTime(8, 0);
+			itemPtr2 -> setEndDate(10, 11);
+			itemPtr2 -> setEndTime(12, 0);
+			itemPtr2 -> setPriority("High");
+			itemPtr2 -> setCategory("Assignment");
+			itemPtr2 -> setVenue("PGP");
+
+			Assert::AreEqual(itemPointer -> addToBank(itemPtr2), true);
+			Assert::AreEqual(itemPointer -> getBankSize(), 2);
+
+			itemPointer -> clearBank();
 		}
 	};
 }
